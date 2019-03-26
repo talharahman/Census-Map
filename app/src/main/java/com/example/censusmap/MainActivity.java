30e8f0ce11af00ceaa00edc12d5a9e1e833cf10b
talharahman <talharahman@pursuit.org>
2019-03-25 21:34:43 -0400

    initial set up of map fragment in main

--- app/src/main/java/com/example/censusmap/MainActivity.java
+++ app/src/main/java/com/example/censusmap/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.censusmap;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public static final int REQUEST_LOCATION_PERMISSION = 123;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.main_map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng nyc = new LatLng(40.7128, -74.0060);
+        googleMap.addMarker(new MarkerOptions().position(nyc).title("Marker in NYC"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(nyc));
+
+        setUISettings();
+
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                ||
+                ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MainActivity.REQUEST_LOCATION_PERMISSION);
+        } else {
+            googleMap.setMyLocationEnabled(true);
+            final FusedLocationProviderClient flpClient = LocationServices.getFusedLocationProviderClient(this);
+            flpClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    final String latLong = "Latitude: " + location.getLatitude() + " Longitude: " + location.getLongitude();
+                    Log.d(TAG, latLong);
+                    Toast.makeText(getApplicationContext(), latLong, Toast.LENGTH_SHORT).show();
+                }
+            });
+            flpClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d(TAG, e.getMessage());
+                }
+            });
+        }
+    }
+
+    private void setUISettings() {
+        UiSettings uiSettings = map.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setAllGesturesEnabled(true);
     }
 }
